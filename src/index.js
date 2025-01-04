059419610c8d52a2a103cc65ab4d43f9c2ab36d6
Megan Lindsay <97409605+mel-121821@users.noreply.github.com>
2025-01-04 07:50:12 -0500

    Add some pseudo code, import pageLoad function and check for functionality

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 //______Index JS File___________
+
+// _________pseudo code______
+
+// 1. Each piece of functionality should be its own module
+// 2. Create content for div#content using JS modules
+// 3. Append each new element to div#content once page is loaded
+// 4. Since we are set up to write code in multiple files, write the initial page-load function in its own module (a seperate file) and call it inside of index.js
+
+
+
 import "./styles.css";
+import { pageLoad } from "./page-load";
 
 console.log("JS is working");
+
+pageLoad();
