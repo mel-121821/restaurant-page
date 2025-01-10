e7acbad1900fa5cc83457e6d6aa4d3393ae34637
Megan Lindsay <97409605+mel-121821@users.noreply.github.com>
2025-01-10 08:00:53 -0500

    Create file and add webpack instruction and local host link

---
+++ src/about.js
@@ -0,0 +1,10 @@
+// ________About Tab________
+
+// use the following command in terminal to update project page:
+// npx webpack serve (Ctrl + C to turn off)
+// Ctrl click url to view updated project
+// http://localhost:8080/
+
+
+
+
---
+++ src/menu.js
@@ -0,0 +1,6 @@
+// ________Menu Tab________
+
+// use the following command in terminal to update project page:
+// npx webpack serve (Ctrl + C to turn off)
+// Ctrl click url to view updated project
+// http://localhost:8080/
